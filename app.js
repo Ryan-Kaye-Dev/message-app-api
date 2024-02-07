2e466586709aa680c968f2c7b7366d6ae8d14da7
bisto.js <ryanrunescapes@outlook.com>
2024-02-07 23:31:17 +0000

    Add Connection to MongoDB

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_DB_URI).then(() => {
+    console.log("Connected to the database successfully!");
+  });
+}
+
+main().catch((err) => console.error(err));
 
 module.exports = app;
