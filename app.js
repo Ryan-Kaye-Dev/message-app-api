29f157f4cad96f0d562081c781156d87114be5d8
bisto.js <ryanrunescapes@outlook.com>
2024-02-07 23:49:22 +0000

    Added CORS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 var indexRouter = require("./routes/index");
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", indexRouter);
 
